fe5c98969a6abbaa3ffbe2a961bca147d92a71de
JorgePCDV <jorge.paredes.cimadevilla@gmail.com>
2025-12-04 22:49:47 +0100

    drawdowns

--- src/advent/get_data/get_data.py
+++ src/advent/get_data/get_data.py
@@ -1,1 +1,6 @@
-init advent
+if __name__ == "__main__":
+    tickers = ["AAPL", "GOOG", "MSFT"]
+    start = "2023-01-01"
+    end = "2024-11-01"
+
+    print("tickers: {}, start: {}, end: {}".format(tickers, start, end))
